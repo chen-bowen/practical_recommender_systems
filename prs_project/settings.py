e4e3ca05638f2f41439da54f457d628b94c7fe3f
Bowen Chen <chenbowen184@gmail.com>
2019-10-13 11:23:18 -0700

    initial commit after migration

--- prs_project/settings.py
+++ prs_project/settings.py
@@ -4,6 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
 
 # Quick-start development settings - unsuitable for production
@@ -68,22 +69,22 @@ WSGI_APPLICATION = 'prs_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'moviegeek',
-#         'USER': 'postgres',
-#         'PASSWORD': 'hullo1!',
-#         'HOST': 'localhost',
-#         'PORT': 5432
-#     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moviegeek',
+        'USER': 'postgres',
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
